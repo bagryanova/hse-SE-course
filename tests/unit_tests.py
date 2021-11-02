619ee7aab49983ad71eb000f5140a2e2a60f446d
Katya Bagryanova <eabagryanova@gmail.com>
2021-11-02 15:33:02 +0300

    Fix test

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -1,10 +1,10 @@
 from app.models import models
 
-now = '2021-09-26T16:29:06.811823'
+NOW = '2021-09-26T16:29:06.811823'
 
 
 def test_create_internship():
-    internship = models.Internship(name="first", description="test 1", updated_at=now, application_num=0, is_open=True)
+    internship = models.Internship(name="first", description="test 1", updated_at=NOW, application_num=0, is_open=True)
     assert internship.name == "first"
     assert internship.description == "test 1"
     assert internship.application_num == 0
@@ -17,7 +17,7 @@ def test_create_user():
 
 
 def test_create_internship_db():
-    internship = models.Internship(name="first", description="test 1", updated_at=now, application_num=0, is_open=True)
+    internship = models.Internship(name="first", description="test 1", updated_at=NOW, application_num=0, is_open=True)
     res_internship = utils.create_internship(db=TestingSessionLocal(), internship=internship)
     assert res_internship.id == 1
     assert res_internship.name == "first"
