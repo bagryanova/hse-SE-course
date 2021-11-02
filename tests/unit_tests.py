e78aec656cbb1718650973e1254f00ce21821c45
OlesyaSub <lesya.sub@mail.ru>
2021-11-02 15:28:19 +0300

    fix style

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -4,7 +4,7 @@ NOW = '2021-09-26T16:29:06.811823'
 
 
 def test_create_internship():
-    internship = models.Internship(name="first", description="test 1", updated_at=now, application_num=0, is_open=True)
+    internship = models.Internship(name="first", description="test 1", updated_at=NOW, application_num=0, is_open=True)
     assert internship.name == "first"
     assert internship.description == "test 1"
     assert internship.application_num == 0
