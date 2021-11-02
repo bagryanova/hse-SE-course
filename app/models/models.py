245a21d941c88960d1a9e55269600cb87dbdc76b
Kira5005-code <anisimova5005@gmail.com>
2021-11-02 14:41:28 +0300

    add impl

---
+++ app/models/models.py
@@ -0,0 +1,13 @@
+import datetime
+from typing import Optional
+
+from pydantic.main import BaseModel
+
+
+class Internship(BaseModel):
+    name: str
+    description: Optional[str] = None
+    updated_at: datetime.datetime
+    application_num: Optional[int] = None
+    is_open: bool
+
