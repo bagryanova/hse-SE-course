3cc964c6071f0e1b3384598276b91ccde9edd2ae
OlesyaSub <lesya.sub@mail.ru>
2021-11-05 16:20:47 +0300

    fix setup

--- setup.sh
+++ setup.sh
@@ -6,4 +6,5 @@ path="$1"
 "$path" -m pip install virtualenv
 "$path" -m virtualenv -p "$path" venv
 source venv/bin/activate
+python3 -m pip install fastapi[all]
 python3 -m pip install -r requirements.txt
